8dea073f5235f8250e4549a12587082e58ae9bcf
QUADEKDEV <quadek50@gmail.com>
2025-08-20 14:50:59 +0100

    updated

--- src/assets/pages/Dashboard.jsx
+++ src/assets/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/sidebar";
+import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 const Dashboard = () => {
   return (
